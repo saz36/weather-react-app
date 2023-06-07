85bfafd43556513d5e2090ae6611f06a83d19c25
saz36 <124158555+saz36@users.noreply.github.com>
2023-06-07 10:22:47 +0100

    cleaned unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer className="App-header">
+       
+        
         <a
-          className="App-link"
+          className="Git-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          open source
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
