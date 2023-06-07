10804b5f0f9a950521efd8dfc3c34c8a57c3ecbb
saz36 <124158555+saz36@users.noreply.github.com>
2023-06-07 11:33:24 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <footer className="App-header">
-        <a
-          className="Git-link"
-          href="https://github.com/saz36/weather-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open source
-        </a>
-      </footer>
+      <div className="container">
+        <footer className="App-header">
+          <a
+            className="Git-link"
+            href="https://github.com/saz36/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open source
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
